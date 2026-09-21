e9ce0414b9c0eec50ef3a73293c193254d45f3c5
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-22 02:12:38 +0600

    delete unnecessary import file

--- src/app/installation/page.tsx
+++ src/app/installation/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { useAppContext } from "@/context/AppContext";
-import getData from "@/lib/getData";
 import { IApp } from "@/types/app.type";
-import { Metadata } from "next";
+
 import Image from "next/image";
 import { ChangeEvent, useMemo, useState } from "react";
 import { FaStar } from "react-icons/fa";
