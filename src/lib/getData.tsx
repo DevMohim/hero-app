b338d733295c58f4d8acbbc8224ddc271b6c3303
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-22 15:32:14 +0600

    add env file

--- src/lib/getData.tsx
+++ src/lib/getData.tsx
@@ -1,7 +1,9 @@
 
 const getData = async () => {
   try {
-    const res = await fetch("http://localhost:3000/data.json" , {cache:'no-store'});
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data.json`, {
+      cache: "no-store",
+    });
     return res.json();
   } catch (err) {
     console.error(err);
